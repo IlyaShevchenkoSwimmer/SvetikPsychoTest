e88d9fa27cccbe9d4dd97be62a44f7e1540dd399
IlyaShevchenkoSwimmer <shevchil@mail.ru>
2025-02-08 22:23:52 +0300

    some fixes for better look

--- src/components/ArrowButtons.tsx
+++ src/components/ArrowButtons.tsx
@@ -9,7 +9,7 @@ function ArrowButtons() {
   return (
     <article className="flex justify-around w-full h-20 mt-8">
       <button
-        className="w-5 h-5 rotate-180"
+        className="w-14 h-14 flex justify-center items-center bg-blue-300 rounded-md rotate-180"
         onClick={() => {
           dispatch(backwardClient());
         }}
@@ -18,12 +18,12 @@ function ArrowButtons() {
         <img
           src="./arrow-right.png"
           alt="arrow-left"
-          className="w-full"
+          className="w-5"
           style={{ opacity: play ? 0.5 : 1 }}
         />
       </button>
       <button
-        className="w-5 h-5"
+        className="w-14 h-14 flex justify-center items-center bg-blue-300 rounded-md"
         onClick={() => {
           dispatch(forwardClient());
         }}
@@ -32,7 +32,7 @@ function ArrowButtons() {
         <img
           src="./arrow-right.png"
           alt="arrow-left"
-          className="w-full"
+          className="w-5"
           style={{ opacity: play ? 0.5 : 1 }}
         />
       </button>
